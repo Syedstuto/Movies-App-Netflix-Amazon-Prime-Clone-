656c206aeba821f857e7ee99a7e8d5b537de7eee
syed <syed@gmail.com>
2026-03-09 09:29:11 +0000

    Solution

--- src/components/AccountSection/index.js
+++ src/components/AccountSection/index.js
@@ -12,34 +12,34 @@ const AccountSection = props => {
   return (
     <>
       <Header />
-      <div className='account-section-bg-container'>
-        <h1 className='account-title'>Account</h1>
-        <hr className='ruler' />
-        <div className='membership-container'>
-          <p className='account-description'>Member ship:</p>
-          <div className='user-details-container'>
-            <p className='account-details'>rahul@gmail.com</p>
-            <p className='account-password'>Password : ************</p>
+      <div className="account-section-bg-container">
+        <h1 className="account-title">Account</h1>
+        <hr className="ruler" />
+        <div className="membership-container">
+          <p className="account-description">Member ship:</p>
+          <div className="user-details-container">
+            <p className="account-details">rahul@gmail.com</p>
+            <p className="account-password">Password : ************</p>
           </div>
         </div>
-        <hr className='ruler' />
-        <div className='membership-container'>
-          <p className='account-description'>Plan details:</p>
-          <p className='account-details'>Premium</p>{' '}
-          <p className='ultra-text'>Ultra HD</p>
+        <hr className="ruler" />
+        <div className="membership-container">
+          <p className="account-description">Plan details:</p>
+          <p className="account-details">Premium</p>{' '}
+          <p className="ultra-text">Ultra HD</p>
         </div>
-        <hr className='ruler' />
-        <div className='button-container'>
+        <hr className="ruler" />
+        <div className="button-container">
           <button
-            className='Logout-button'
-            type='button'
+            className="Logout-button"
+            type="button"
             onClick={onClickLogout}
           >
             Logout
           </button>
         </div>
       </div>
-      <div className='account-footer'>
+      <div className="account-footer">
         <FooterSection />
       </div>
     </>
--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -75,7 +75,7 @@ class HomeSection extends Component {
   }
 
   renderPosterLoadingView = () => (
-    <div className="home-loader-container" testid="loader">
+    <div className="home-loader-container">
       <Loader type="TailSpin" color="#D81F26" height={50} width={50} />
     </div>
   )
